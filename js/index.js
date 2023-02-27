66e30fb058c1d4a5f69fb044b919afa9bde6a068
wowjd15978 <125331860+wowjd15978@users.noreply.github.com>
2023-02-27 18:06:58 +0900

    0227 2차 weather

--- js/index.js
+++ js/index.js
@@ -6,4 +6,36 @@ $(function(){
        centerMode: true,
        focusOnSelect: true
     });
+
+    $('#searchbtn').on('click', function(){
+        if($('.searchinput').hasClass('act')){
+           $('.searchinput').removeClass('act'); 
+        }else{
+           $('.searchinput').addClass('act');
+           $('.searchinput input').focus();
+        }
+    });
+
+    $('#search-city').on('keypress', function(e){
+        if(e.which == 13 && !e.shiftkey){
+            const key =$(this).val();
+            $(this).val('');
+            $('.searchinput').removeClass('act');
+        }
+    });
+
+    
+
  }); //jquery
+
+ function getWeatherLocation(){
+    navigator.geolocation.getCurrentPosition(
+        onWeatherSuccess, onWeatherError, {enableHighAccuracy: true}
+    );
+ };
+
+ const onWeatherSuccess = function(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    
+};
